bc0343e9c279b1dce76925bb2e1215efc6148132
Prem-ium <thefrostscoper@gmail.com>
2023-04-19 10:20:50 -0400

    Fix Echo Typo(s)

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main():
             FILES.append(info["file"])
             print(f"{var_name} is enabled.")
             if var_name == "SCHWAB_AI":
-                info["task"] = "return Array.from({length: " + os.environ[var_name] + "}, (_, i) => i);"
+                info["task"] = "Array.from({length: " + os.environ[var_name] + "}, (_, i) => i);"
             elif var_name == "MERRILL_AI":
                 info["task"] = [i + 1 for i in range(int(os.environ[var_name].replace(" ", "").replace(",", ", ")))]
             else:
@@ -60,7 +60,6 @@ def main():
                 if command['command'] == 'executeScript' and command['value'] == 'accounts':
                     command['target'] = f"return {task}"
                     break
-
 
         filePath = filePath.replace("src\\Selenium_IDE\\", "")
         filePath = filePath.replace("src\\X_Archive\\", "")
