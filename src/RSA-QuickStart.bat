da4fdaa56a81d3be1b991ddee6e8ec973b5283db
Prem-ium <thefrostscoper@gmail.com>
2023-04-25 14:45:38 -0400

    Browser Quickstart

--- src/RSA-QuickStart.bat
+++ src/RSA-QuickStart.bat
@@ -5,3 +5,10 @@ REM REPLACE THE PATH BELOW WITH YOUR OWN PATH TO THE PROJECT FOLDER
 cd C:\Users\Frost\Desktop\CodingProjects\Auto-StockTrader
 
 python main.py %ticker%
+
+REM Examples of quickstarting with different browsers and profiles
+REM Right click on browser-> properties-> target to find the path to the browser
+
+REM start msedge.exe --profile-directory="Default"
+REM start brave.exe --profile-directory="Default"
+REM start brave.exe --profile-directory="Profile 1"
