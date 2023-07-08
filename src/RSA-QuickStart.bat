0631194ada6af09f9ae5c4f4957db4dd5bcb89b0
Prem-ium <thefrostscoper@gmail.com>
2023-07-08 08:54:31 -0400

    Ask to Open Browsers

--- src/RSA-QuickStart.bat
+++ src/RSA-QuickStart.bat
@@ -8,7 +8,12 @@ python main.py %ticker%
 
 REM Examples of quickstarting with different browsers and profiles
 REM Right click on browser-> properties-> target to find the path to the browser
+REM Remove 'REM' from the lines below to enable them.
 
-REM start msedge.exe --profile-directory="Default"
-REM start brave.exe --profile-directory="Default"
-REM start brave.exe --profile-directory="Profile 1"
+REM set /p open_browsers=Do you want to open browsers? (y/n): 
+
+REM if "%open_browsers%"=="y" (
+REM    start msedge.exe --profile-directory="Default"
+REM    start brave.exe --profile-directory="Default"
+REM    start brave.exe --profile-directory="Profile 1"
+)
